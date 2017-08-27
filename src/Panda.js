b9c0b73dd042b6957eb2ccd1cce278a59540ed2b
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-27 19:06:04 +0300

    Fix panda version

--- src/Panda.js
+++ src/Panda.js
@@ -16,7 +16,7 @@ var Panda = Panda || {};
      * @type {void|Object|*}
      */
     Panda = $.extend(true, Panda || {}, {
-        version: '1.2.0',
+        version: '1.2.1',
 
         /**
          * Initialize all Packages
