a102364e4db8550d84d409574daa7b390ca2bf0f
Ioannis Papikas <papikas.ioan@gmail.com>
2018-02-23 22:58:20 +0200

    Add comments in Service functions

--- src/panda/Events.js
+++ src/panda/Events.js
@@ -6,10 +6,22 @@
      * @type {void|Object|*}
      */
     Panda.Events = $.extend(Panda.Events || {}, {
+        /**
+         * Initialize the Panda Events Library
+         */
         init: function () {
             // Register global events
             Panda.Events.Library.init();
         },
+
+        /**
+         * Add an event listener using jQuery's 'on'
+         *
+         * @param {object|string} listener
+         * @param {string} event
+         * @param {string} context
+         * @param {function} callback
+         */
         on: function (listener, event, context, callback) {
             $(listener).on(event, context, function () {
                 if (typeof callback === 'function') {
@@ -17,6 +29,15 @@
                 }
             });
         },
+
+        /**
+         * Add an event listener using jQuery's 'one'
+         *
+         * @param {object|string} listener
+         * @param {string} event
+         * @param {string} context
+         * @param {function} callback
+         */
         one: function (listener, event, context, callback) {
             $(listener).one(event, context, function () {
                 if (typeof callback === 'function') {
@@ -24,9 +45,25 @@
                 }
             });
         },
+
+        /**
+         * Remove an event listener.
+         *
+         * @param {object|string} listener
+         * @param {string} event
+         * @param {string} context
+         */
         off: function (listener, event, context) {
             $(listener).off(event, context);
         },
+
+        /**
+         * Trigger a specific event
+         *
+         * @param {object|string} listener
+         * @param {string} event
+         * @param {function} callback
+         */
         dispatch: function (listener, event, callback) {
             $(listener).trigger(event, callback);
         }
