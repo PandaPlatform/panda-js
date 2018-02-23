6564c964850052832b85e1ae1b8379a76bedc51a
Ioannis Papikas <papikas.ioan@gmail.com>
2018-02-23 22:56:54 +0200

    Add comments in Service functions

--- src/panda/env/Url.js
+++ src/panda/env/Url.js
@@ -6,62 +6,11 @@
      * @type {void|Object|*}
      */
     Panda.Env.Url = $.extend(true, Panda.Env.Url || {}, {
-        getVar: function (name) {
-            return Panda.Env.Url.getUrlVar(window.location.href, name);
-        },
-        getUrlVar: function (url, name) {
-            var vars = {};
-            url = url.split('#')[0];
-            url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-                if (key in vars) {
-                    if (typeof vars[key] === "string") {
-                        vars[key] = [vars[key]];
-                    }
-                    vars[key] = $.merge(vars[key], [value]);
-                } else
-                    vars[key] = value;
-            });
-
-            if (typeof(name) !== "undefined") {
-                return vars[name];
-            }
-
-            return vars;
-        },
-        removeVar: function (hrf, vrb) {
-            // If URL has no variables, return it
-            if (hrf.indexOf("?") === -1) {
-                return hrf;
-            }
-
-            // Split variables from URI
-            var hr_splitted = hrf.split("?");
-            var prefix = hr_splitted[0];
-            var vrbles_sec = "?" + hr_splitted[1];
-
-            // Remove variable using patterns
-            var var_patt = new RegExp(vrb + "=(?=[^&]*)[^&]*[&*]|[&*]" + vrb + "=(?=[^&]*)[^&]*|^\\?" + vrb + "=(?=[^&]*)[^&]*$", "i");
-            vrbles_sec = vrbles_sec.replace(var_patt, "");
-            return prefix + vrbles_sec;
-        },
-        redirect: function (url) {
-            // If site is trusted
-            window.location = url;
-            window.location.href = url;
-        },
-        reload: function (forceGet) {
-            forceGet = (typeof forceGet === 'undefined') ? false : forceGet;
-            window.location.reload(forceGet);
-        },
-        getSubdomain: function () {
-            var info = this.info();
-            return info.sub;
-        },
-        getDomain: function () {
-            var info = this.info();
-            return info.domain;
-
-        },
+        /**
+         * Get all information about the current url
+         *
+         * @return {{protocol: string, sub: string, domain: string, pathname: *, hash: string}}
+         */
         info: function () {
             // Get init variables
             var fullHost = window.location.host;
@@ -74,34 +23,126 @@
                 parts = parts.splice(1);
             }
 
-            var info = {};
-            info['protocol'] = window.location.protocol.replace(":", "");
-            info['sub'] = sub;
-            info['domain'] = parts.join(".");
-            info['pathname'] = window.location.pathname;
-            info['hash'] = window.location.hash.substr(1);
-
-            return info;
+            // Create object of all url information
+            return {
+                protocol: window.location.protocol.replace(':', ''),
+                sub: sub,
+                domain: parts.join('.'),
+                pathname: this.getPathname(),
+                hash: window.location.hash.substr(1)
+            };
         },
+
+        /**
+         * Get the current url's pathname
+         *
+         * @return {string}
+         */
         getPathname: function () {
             return encodeURIComponent(window.location.pathname);
         },
-        resolve: function (sub, url) {
-            // Check if the url is already resolved
-            if (url.indexOf("http") === 0) {
+
+        /**
+         * Get the current url's domain
+         *
+         * @return {string}
+         */
+        getDomain: function () {
+            var info = this.info();
+            return info.domain;
+
+        },
+
+        /**
+         * Get the current url's sub-domain
+         *
+         * @return {string}
+         */
+        getSubDomain: function () {
+            var info = this.info();
+            return info.sub;
+        },
+
+        /**
+         * Get a variable value from the current url
+         *
+         * @param {string} name
+         * @return {string}
+         */
+        getVar: function (name) {
+            return Panda.Env.Url.getUrlVar(window.location.href, name);
+        },
+
+        /**
+         * Get a variable value from a given url.
+         * If no variable is found for the given name, return all variables.
+         *
+         * @param {string} url
+         * @param {string} name
+         * @return {string|object}
+         */
+        getUrlVar: function (url, name) {
+            var vars = {};
+            url = url.split('#')[0];
+            url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+                if (key in vars) {
+                    if (typeof vars[key] === 'string') {
+                        vars[key] = [vars[key]];
+                    }
+                    vars[key] = $.merge(vars[key], [value]);
+                } else
+                    vars[key] = value;
+            });
+
+            if (typeof(name) !== 'undefined') {
+                return vars[name];
+            }
+
+            return vars;
+        },
+
+        /**
+         * Remove a variable from a given url
+         *
+         * @param {string} url
+         * @param {string} name
+         * @return {string}
+         */
+        removeVar: function (url, name) {
+            // If URL has no variables, return it
+            if (url.indexOf('?') === -1) {
                 return url;
             }
 
-            // Check the subdomain
-            var urlInfo = this.info();
-            var urlProtocol = urlInfo['protocol'];
-            var resolved_url = this.getDomain() + "/" + url;
-            resolved_url = (sub === "www" ? "" : sub + ".") + resolved_url;
-            return urlProtocol + "://" + resolved_url;
+            // Split variables from URI
+            var path_parts = url.split('?');
+            var prefix = path_parts[0];
+            var variables_part = '?' + path_parts[1];
+
+            // Remove variable using patterns
+            var var_pattern = new RegExp(name + '=(?=[^&]*)[^&]*[&*]|[&*]' + name + '=(?=[^&]*)[^&]*|^\\?' + name + '=(?=[^&]*)[^&]*$', 'i');
+            variables_part = variables_part.replace(var_pattern, '');
+            return prefix + variables_part;
         },
-        resource: function (url) {
-            // Resolve url to www
-            return this.resolve("www", url);
+
+        /**
+         * Redirect user to the given url
+         *
+         * @param {string} url
+         */
+        redirect: function (url) {
+            window.location = url;
+            window.location.href = url;
+        },
+
+        /**
+         * Reload the current page
+         *
+         * @param {boolean} forceGet
+         */
+        reload: function (forceGet) {
+            forceGet = (typeof forceGet === 'undefined') ? false : forceGet;
+            window.location.reload(forceGet);
         }
     });
 })(jQuery);
